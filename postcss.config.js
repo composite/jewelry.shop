d5931608d2f441067af01aedab9e38fb0de99f45
composite <ukjinplant@msn.com>
2020-11-03 17:22:16 +0900

    css env scss

--- postcss.config.js
+++ postcss.config.js
@@ -2,18 +2,22 @@
 
 const tailwind = require('tailwindcss');
 const cssnano = require('cssnano');
-const postcssImport = require('postcss-import');
-const autoprefixer = require('autoprefixer');
+const postcssImport = require('postcss-import')({
+    plugins: [],
+    path: ['./node_modules'],
+});
 const presetEnv = require('postcss-preset-env')({
+  autoprefixer: { },
   features: {
     // enable nesting
     'nesting-rules': true,
   },
 });
+const syntax = 'postcss-scss'
 
 const plugins =
   process.env.NODE_ENV === 'production'
-    ? [postcssImport, tailwind, presetEnv, cssnano, autoprefixer]
-    : [postcssImport, tailwind, presetEnv/*, autoprefixer*/];
+    ? [postcssImport, tailwind, presetEnv, cssnano]
+    : [postcssImport, tailwind, presetEnv];
 
-module.exports = { plugins };
+module.exports = { syntax, plugins };
--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,9 @@ module.exports = {
     scss: {
       includePaths: ["src"],
     },
-    postcss: true
+    postcss: true,
+    defaults: {
+      style: 'scss'
+    }
   })
 };
