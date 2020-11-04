65cd4039f71258e8a8293e326b7ddf6ca25597d4
composite <ukjinplant@msn.com>
2020-11-04 20:06:04 +0900

    logo and start

---
+++ src/components/Logo.svelte
@@ -0,0 +1,13 @@
+<style>
+.logo {
+  display: initial;
+  background: #7f0080;
+  background: linear-gradient(45deg, #ad5389, #3c1053, #ad5389);
+  background-size: 400% 400%;border-radius: 50%;
+  animation: 1s ease infinite paused alternate logobganim;
+}
+.logo:hover {
+  animation: 1s ease infinite alternate logobganim;
+}
+</style>
+<img alt="" class:logo={true} src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='256' height='256'%3E%3Cg%3E%3Cpath d='M186.89 44H224v124.48q0 24.413-4.026 37.559-5.409 17.17-19.624 27.632Q186.134 244 162.86 244q-27.298 0-42.017-16.23-14.718-16.366-14.844-47.888l35.098-4.292q.63 16.9 4.655 23.876 6.038 10.597 18.366 10.597 12.455 0 17.612-7.512 5.158-7.646 5.158-31.522z' fill='%23fff'/%3E%3C/g%3E%3C/svg%3E" {...$$restProps} >
--- src/routes/layout/default.svelte
+++ src/routes/layout/default.svelte
@@ -1,8 +1,20 @@
 <script>
   import Nav from "../../components/Nav.svelte";
+  import Logo from "../../components/Logo.svelte";
 
   export let segment;
 </script>
+
+<nav class="flex">
+  <div class="flex-auto">
+    <Logo width="32" height="32" class="align-middle" />
+    Server
+  </div>
+  <div class="flex-grow"></div>
+  <div class="flex-auto">
+    asd
+  </div>
+</nav>
 
 <Nav {segment} />
 
