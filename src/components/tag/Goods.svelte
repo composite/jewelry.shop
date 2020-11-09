e2ec5110ffeae65b25aab0ee13d6fa02256febd9
composite <ukjinplant@msn.com>
2020-11-09 22:12:00 +0900

    example image

--- src/components/tag/Goods.svelte
+++ src/components/tag/Goods.svelte
@@ -2,47 +2,13 @@
   export let seq;
 </script>
 <style>
-  @screen xs {
-    article {
-      @apply p-1 flex-auto;
-    }
-  }
-
-  @screen sm {
-    article {
-      @apply p-1;
-
-      flex: 0 0 50%;
-    }
-  }
-
-  @screen md {
-    article {
-      @apply p-2;
-
-      flex: 0 0 33.333333%;
-    }
-  }
-
-  @screen lg {
-    article {
-      @apply p-3;
-
-      flex: 0 0 25%;
-    }
-  }
-
-  @screen xl {
-    article {
-      @apply p-4;
-
-      flex: 0 0 20%;
-    }
+  article {
+    @apply flex-auto m-1;
   }
 </style>
 <article>
   <figure class="mx-auto text-center">
-    <img alt="Jewelry Name {seq}" src="https://picsum.photos/seed/{seq}/256/256" >
+    <img alt="Jewelry Name {seq}" src="https://loremflickr.com/240/320/diamond,ring/all?lock={seq}" width="100%" height="100%" >
     <figcaption>Jewelry Name {seq}</figcaption>
   </figure>
 </article>
