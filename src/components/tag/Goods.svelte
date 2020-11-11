0ab506f0a628c7ef7cc1381856e5d860d6f80b6f
composite <ukjinplant@msn.com>
2020-11-11 22:23:15 +0900

    goods grid design

--- src/components/tag/Goods.svelte
+++ src/components/tag/Goods.svelte
@@ -1,10 +1,15 @@
 <script>
-  import Fa from 'svelte-fa'
-  import { faHeart } from '@fortawesome/free-regular-svg-icons'
+  import Fa from "svelte-fa";
+  import { faHeart as heartOff } from "@fortawesome/free-regular-svg-icons";
+  import {
+    faHeart as heartOn,
+    faCartPlus,
+  } from "@fortawesome/free-solid-svg-icons";
 
   export let seq;
   export let info;
 </script>
+
 <style>
   article {
     --goods-width: 240px;
@@ -15,15 +20,58 @@
     background-size: cover;
     background-position: center;
 
-    @apply flex-auto m-1 rounded-md relative;
+    @apply flex-auto m-1 rounded-md relative cursor-pointer;
+  }
+
+  article > span {
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: opacity 0.25s;
+    line-height: 1;
+
+    @apply opacity-0 block absolute top-0 left-0 m-2 p-1 text-white cursor-default;
+  }
+
+  article:hover > span {
+    @apply opacity-100;
   }
 
   article > button {
     font-size: 0.85rem;
     background-color: rgba(0, 0, 0, 0.5);
-    padding: 0.5rem 0.6rem;
+    line-height: 1;
+    transition-duration: 0.5s;
+    transition-property: background-color, color;
 
-    @apply rounded-full absolute right-0 m-2 text-white;
+    @apply rounded-full absolute right-0 m-2 p-2 text-white;
+  }
+
+  article > button:hover {
+    background-color: rgba(255, 255, 255, 0.5);
+
+    @apply text-black;
+  }
+
+  article > div {
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: opacity 0.25s;
+
+    @apply opacity-0 block absolute bottom-0 left-0 right-0 h-0 px-2 py-4 text-white overflow-hidden cursor-auto;
+  }
+
+  article:hover > div {
+    @apply opacity-100 h-auto;
+  }
+
+  article > div > button {
+    border: 1px solid white;
+    font-size: 0.85rem;
+    line-height: 1;
+
+    @apply rounded-full text-white float-right m-2 p-2;
+  }
+
+  article > div > h4 {
+    text-overflow: ellipsis;
   }
 
   @screen ln {
@@ -32,7 +80,14 @@
     }
   }
 </style>
-<article style="background-image: url(https://loremflickr.com/240/320/diamond,ring/all?lock={seq})">
-  <button><Fa icon={faHeart} /></button>
-  Jewelry Name {seq}
+
+<article
+  style="background-image: url(https://loremflickr.com/240/320/diamond,ring/all?lock={seq})">
+  <span>&dollar; 123.45</span>
+  <button type="button"><Fa icon={heartOff} /></button>
+  <div>
+    <button type="button"><Fa icon={faCartPlus} /></button>
+    <h4 class="text-xl font-bold">Jewelry Name {seq}</h4>
+    <h5 class="text-sm">asd</h5>
+  </div>
 </article>
--- src/routes/layout/default.svelte
+++ src/routes/layout/default.svelte
@@ -1,19 +1,43 @@
 <script>
+  import Fa from "svelte-fa";
+  import { faUser } from "@fortawesome/free-regular-svg-icons";
+  import { faBars } from "@fortawesome/free-solid-svg-icons";
+
   import ChipStore from "../../components/common/ChipStore.svelte";
   import Logo from "../../components/Logo.svelte";
 </script>
+
 <style>
   nav {
     @apply p-2 m-2 border-b border-primary;
   }
+
+  button {
+    padding: 8px;
+    font-size: 16px;
+    line-height: 1;
+    transition-duration: 0.5s;
+    transition-property: background-color;
+
+    @apply rounded-full;
+  }
+
+  button:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
 </style>
+
 <nav class="flex">
   <div>
+    <button type="button"><Fa icon={faBars} /></button>
+  </div>
+  <div class="flex-grow text-center">
+    Jewelry
     <Logo width="32" height="32" class="align-middle" />
     Server
   </div>
-  <div class="flex-grow"></div>
   <div>
+    <button type="button"><Fa icon={faUser} /></button>
   </div>
 </nav>
 <ChipStore />
