0a527f070e10b8f5b6b0d29a98cfe609b258e70a
composite <ukjinplant@msn.com>
2021-02-10 17:54:55 +0900

    마이그 준비 1

--- postcss.config.js
+++ postcss.config.js
@@ -1,43 +1,21 @@
 // postcss.config.js
 
-const defaultWhitelist = ["html", "body", "stroke-primary", "mode-dark"];
-const defaultWhitelistPatterns = [
-  // for JS ripple
-  /ripple/,
-  // date picker
-  /w\-.\/7/,
-  /svelte-/
-];
-
 const tailwind = require('tailwindcss');
-const cssnano = require('cssnano');
-const url = require("postcss-url")();
-const inputRange = require("postcss-input-range")();
-const postcssImport = require('postcss-import')({
-    plugins: [],
-    path: ['./node_modules'],
-});
+const flexbugs_fixes = require('postcss-flexbugs-fixes');
 const presetEnv = require('postcss-preset-env')({
-  autoprefixer: { },
-  features: {
-    // enable nesting
-    'nesting-rules': true,
+  autoprefixer: {
+    flexbox: 'no-2009',
+    grid: 'autoplace'
   },
+  stage: 3,
+  features: {
+    'custom-properties': false
+  }
 });
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './src/**/*.html',
-    './src/**/*.svelte'
-  ],
-  whitelist: [...defaultWhitelist],
-  whitelistPatterns: [...defaultWhitelistPatterns],
-  whitelistPatternsChildren: [...defaultWhitelistPatterns],
 
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-});
 const plugins =
-  process.env.NODE_ENV === 'production'
-    ? [postcssImport, url, inputRange, tailwind, presetEnv, cssnano, purgecss]
-    : [postcssImport, url, inputRange, tailwind, presetEnv];
+  process.env.NODE_ENV === 'development'
+    ? [tailwind]
+    : [tailwind, flexbugs_fixes, presetEnv];
 
 module.exports = { plugins };
